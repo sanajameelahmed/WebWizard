643455890db8b573758d0dcef28ebfe6978c6c16
Sana Jameel Ahmed <50713082+sanajameelahmed@users.noreply.github.com>
2025-06-30 22:16:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+  const images = document.querySelectorAll("#slider img");
+  let current = 0;
+
+  function showImage(index) {
+    images.forEach((img, i) => {
+      img.classList.toggle("active", i === index);
+    });
+  }
+
+  function nextImage() {
+    current = (current + 1) % images.length;
+    showImage(current);
+  }
+
+  function prevImage() {
+    current = (current - 1 + images.length) % images.length;
+    showImage(current);
+  }
